feb220a4828c28740e19111ca5839fee9c0f4eef
Alekssandher <alekssandher1@hotmail.com>
2024-10-15 18:50:43 -0300

    updates

--- Desafio 1/index.js
+++ Desafio 1/index.js
@@ -10,10 +10,17 @@ checkFibonacci(valor)
 
 function checkFibonacci(valor) {
     let fibonacci = [0, 1]
+    let num
+    let num1 = 0
+    let num2 = 1
 
     for (let i = 2; i < valor; i++) {
         
-        fibonacci.push((i - 1) + (i - 2))
+        num = num1 + num2
+        num2 = num1
+        num1 = num
+
+        fibonacci.push(num)
 
         if (fibonacci.includes(valor)){
             return console.log(`O valor digitado existe na tabela fibonacci: \n ${fibonacci}`)
