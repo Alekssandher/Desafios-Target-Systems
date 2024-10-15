31daed11427e436bdfe9eb8604bfefa7e89eec70
Alekssandher <alekssandher1@hotmail.com>
2024-10-15 18:26:49 -0300

    updates

---
+++ Desafio 2/index.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')()
+
+var text = ""
+var textTransformed = ""
+var textArray = []
+
+
+console.log("-------VERIFICADORA DA LETRA A-------")
+console.log("Digite um texto para verificar ")
+text = prompt()
+textTransformed = text.toLowerCase()
+
+checkText(textTransformed)
+
+function checkText(textTransformed) {
+    
+    textArray = textTransformed.split('')
+    let timesAppeared = textArray.filter(letra => letra === "a").length
+    return console.log(`A letra a apareceu ${timesAppeared} vezes na frase: ${text}`)
+}
